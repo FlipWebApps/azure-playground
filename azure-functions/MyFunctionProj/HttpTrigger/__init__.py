e7b77cbdd9ad72de3e870dfc635d0c9a1b693759
Mark Hewitt <MHEW@equinor.com>
2019-06-25 10:40:57 +0200

    API additions for REST

--- azure-functions/MyFunctionProj/HttpTrigger/__init__.py
+++ azure-functions/MyFunctionProj/HttpTrigger/__init__.py
@@ -4,8 +4,21 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest, msg: func.Out[func.QueueMessage]) -> str:
+    # For testing we log the content
+    logging.info(f"HTTP trigger executed!")
+    logging.info(f"Headers: {req.headers}")
+    logging.info(f"Params: {req.params}")
+    logging.info(f"Route Params: {req.route_params}")
+    logging.info(f"Body: {req.get_body()}")
+    try:
+        logging.info(f"Body JSON: {request.get_json()}")
+    except ValueError:
+        pass
 
     name = req.params.get('name')
+    category = req.route_params.get('category')
+    id = req.route_params.get('id')
+
     if not name:
         try:
             req_body = req.get_json()
@@ -15,7 +28,10 @@ def main(req: func.HttpRequest, msg: func.Out[func.QueueMessage]) -> str:
             name = req_body.get('name')
 
     if name:
+        # send a message to the QueueMessage output binding
         msg.set(name)
+
+        # Return the HttpResponse
         return func.HttpResponse(f"Hello to {name}!")
     else:
         return func.HttpResponse(
--- python-webapp/README.md
+++ python-webapp/README.md
@@ -19,4 +19,14 @@ pytest -q test_wallet.py
 ```
 
 ## Azure DevOps Pipeline
-Configure a new pipeline to reference this repository and the included azure-pipelines.yml
+Configure a new pipeline to reference this repository and the included azure-pipelines.yml:
+* Select Builds -> New
+* Select the *classic editor* option
+* Select *GitHub* and chose a repository
+* Select the template *Configuration as code -> YAML*
+* Set the YAML file path to python-webapp/azure-pipelines.yaml
+* Fill out variables:
+	* ConnectedServiceName - 
+	* WebAppName - The name of your web app
+
+This pipeline will build and deploy as one step.
